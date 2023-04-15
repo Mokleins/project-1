99d264024c70d82e54bbb2e9cfb313673f3a5ee8
Xylon Funes <xyfullns@gmail.com>
2023-04-14 23:54:43 -0400

    create Button and display data

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
-//Creating an array to store verses data
-const versesArray = [];
-//Calling the API, and Passing the URL
-const linkOfBible = "https://labs.bible.org/api/?passage=random&type=json";
-for (let i = 0; i < 5; i++) {
-  fetch(linkOfBible, {
-    method: "GET",
-    // headers: {
-    //     "api-key": 'e1588741b7d4e14dea4d9cbe3b5eea55'
+//Adding the event Listener to DOMCOntentloaded Event
+document.addEventListener("DOMContentLoaded", () => {
+  //Creating an array to store verses data
+  const versesArray = [];
 
-    // }
-  })
-    .then((data) => data.json())
-    .then((bibleData) => versesArray.push(bibleData))
-    .catch((error) => console.log(error));
-}
-//The function will render the data from the versesArray
-  console.log(versesArray)
+  //Calling the API, and Passing the URL
+  const linkOfBible = "https://labs.bible.org/api/?passage=random&type=json";
 
-  console.log(versesArray.map(verses => (versesArray)));
+  for (let i = 0; i < 5; i++) {
+    fetch(linkOfBible)
+      .then((data) => data.json())
+      .then((bibleData) => {
+        versesArray.push(bibleData);
+        if (i === 4) {
+          render(versesArray);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
 
-  
-//The function renders the data
-const render = (bibleData) => {
-  const versesContainer = document.querySelector(".verses-container");
-  const verseCard = document.createElement("div");
-  const nameBible = document.createElement("h4");
-  const bibleP = document.createElement("p");
-  nameBible.textContent =
-    bibleData[0].bookname +
-    " " +
-    bibleData[0].chapter +
-    ":" +
-    bibleData[0].verse;
-  bibleP.innerHTML = bibleData[0].text;
-  console.log(bibleData);
-  verseCard.append(nameBible, bibleP);
-  versesContainer.append(verseCard);
-};
+  //The function renders the data
+  const render = (versesArray) => {
+    const button = document.createElement("button");
+    versesArray.forEach((verse) => {
+      const versesContainer = document.querySelector(".verses-container");
+      const verseCard = document.createElement("div");
+      const nameBible = document.createElement("h4");
+      const bibleP = document.createElement("p");
+      nameBible.textContent =
+        verse[0].bookname + " " + verse[0].chapter + ":" + verse[0].verse;
+      bibleP.innerHTML = verse[0].text;
+      verseCard.append(nameBible, bibleP);
+      versesContainer.append(verseCard,button);
+    });
+  };
 
-//Create an event listener that instead reloading the page the user can click to get a new verse.
+  //Create an event listener that instead reloading the page the user can click to get a new verse.
+});
